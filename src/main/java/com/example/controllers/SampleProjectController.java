11dc75743483687f31406ced173a5405231d97cc
Ben Sterrett <raynjamin@gmail.com>
2017-01-23 12:44:39 -0500

    Adding sample business logic

--- src/main/java/com/example/controllers/SampleProjectController.java
+++ src/main/java/com/example/controllers/SampleProjectController.java
@@ -2,32 +2,27 @@ package com.example.controllers;
 
 import com.example.entities.Game;
 import com.example.services.GameRepository;
+import com.example.viewmodels.GameViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 /**
  * Created by Ben on 1/21/17.
  */
 
-@Controller
+@RestController
 public class SampleProjectController {
 
     @Autowired
     GameRepository games;
 
-    @RequestMapping(path = "/", method = RequestMethod.GET)
-    public String homePage(Model model) {
-        model.addAttribute("games", games.findAll());
-        return "home";
-    }
+    @CrossOrigin
+    @RequestMapping(path = "/", method = RequestMethod.POST)
+    public GameViewModel homePage(@RequestBody Game newGame) {
+        games.save(newGame);
 
-    @RequestMapping(path = "/add-game", method = RequestMethod.POST)
-    public String postGame(Game game) {
-
-        games.save(game);
-        return "redirect:/";
+        return new GameViewModel((List)games.findAll());
     }
 }
